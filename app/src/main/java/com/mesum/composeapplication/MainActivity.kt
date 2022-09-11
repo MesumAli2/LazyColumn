4ff8c7e90b9bc35e287d598d726543a3d3f6d93a
MesumAli2 <smesumali20@gmail.com>
2022-09-11 14:57:13 +0400

    Implementing styling by adding custom day and night theme.

--- app/src/main/java/com/mesum/composeapplication/MainActivity.kt
+++ app/src/main/java/com/mesum/composeapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mesum.composeapplication
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,8 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mesum.composeapplication.ui.theme.ComposeApplicationTheme
@@ -53,7 +56,7 @@ fun MainApp(){
 
 @Composable
 fun OnBoardingScreen(onContinueClicked:() -> Unit) {
-androidx.compose.material.Surface {
+Surface {
     Column(modifier = Modifier.fillMaxSize(),
     verticalArrangement = Arrangement.Center
     , horizontalAlignment = Alignment.CenterHorizontally) {
@@ -111,7 +114,7 @@ private fun Greeting(name: String) {
 
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name, style =  MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold ))
             }
 
             OutlinedButton(onClick = { expanded.value = !expanded.value }) {
@@ -122,10 +125,13 @@ private fun Greeting(name: String) {
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES,  name = "DefaultPreviewDark" )
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeApplicationTheme {
         Greeting("Android")
     }
+
+
 }
