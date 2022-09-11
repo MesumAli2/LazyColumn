5422ce72a18e5782049005c1fdb9e29cc224dc6b
MesumAli2 <smesumali20@gmail.com>
2022-09-11 14:03:32 +0400

    Added softness animation to provide realistic spring like animation

--- app/src/main/java/com/mesum/composeapplication/MainActivity.kt
+++ app/src/main/java/com/mesum/composeapplication/MainActivity.kt
@@ -3,11 +3,15 @@ package com.mesum.composeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,7 +38,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainApp(){
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable{ mutableStateOf(true) }
 
     if (shouldShowOnboarding){
 
@@ -87,7 +91,12 @@ private fun Greeting(name: String) {
     val expanded = remember {
         mutableStateOf(false)
     }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    //val extraPadding = if (expanded.value) 48.dp else 0.dp
+    
+    val extraPadding by animateDpAsState(if (expanded.value) 48.dp else 0.dp, animationSpec = spring(dampingRatio = Spring.DampingRatioLowBouncy,
+        stiffness = Spring.StiffnessLow
+        ))
+    
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -98,7 +107,7 @@ private fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
 
             ) {
                 Text(text = "Hello, ")
