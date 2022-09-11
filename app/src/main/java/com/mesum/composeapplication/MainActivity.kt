ff33517a081a8a9db1375e1f431e8f1ffcc17b08
MesumAli2 <smesumali20@gmail.com>
2022-09-11 15:37:00 +0400

    Removed text and added show less and more icon instead, provided enhanced bouncy effect

--- app/src/main/java/com/mesum/composeapplication/MainActivity.kt
+++ app/src/main/java/com/mesum/composeapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -11,10 +12,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -91,7 +96,7 @@ fun MyApp(names: List<String> = List(1000) { "$it" }
 
 @Composable
 private fun Greeting(name: String) {
-    val expanded = remember {
+    var expanded = remember {
         mutableStateOf(false)
     }
     //val extraPadding = if (expanded.value) 48.dp else 0.dp
@@ -104,22 +109,34 @@ private fun Greeting(name: String) {
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(modifier = Modifier.padding(24.dp).animateContentSize (animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow))) {
 
 
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                    .padding(12.dp)
 
             ) {
                 Text(text = "Hello, ")
                 Text(text = name, style =  MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold ))
+                if (expanded.value){
+                    Text(text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4))
+                }
             }
 
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "Show less" else "Show More")
+            IconButton(onClick = { expanded .value= !expanded.value }) {
+                Icon(
+                    imageVector = if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded.value) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
 
+                )
             }
         }
     }
